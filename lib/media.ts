c43922a27120e58646a2ff845df68d90b0044889
Abdulazeez A. Noaman <42905848+HostX0@users.noreply.github.com>
2026-08-17 00:04:40 +0300

    feat: add Pearl 2026 visual asset registry

---
+++ lib/media.ts
@@ -0,0 +1,61 @@
+const driveImage = (id: string, width = 2400) => `https://drive.google.com/thumbnail?id=${id}&sz=w${width}`;
+
+export const media = {
+  logos: {
+    ar: driveImage('1h1CF6l-ZpZSII7upncjUIC01TAhn4-L_', 1400),
+    en: driveImage('1OC7oAaf306UyZALXExnlJGNjX9RpLpSt', 1400),
+  },
+  home: {
+    hero: driveImage('1IQwt-uBSp5BPB1AY-E7ei4Ik4Sak9KoP'),
+    carton: driveImage('1X6T5UP9FuxZP3KwFL_wamSr5fcl6cf76'),
+    ripple: driveImage('1fLaoyhad38gjFsKbvpRQApj2TmXLqCll'),
+    purity: driveImage('1gi8HKhcBgPnSzzOhXsPQdZOHGw-UO0wS'),
+    process: driveImage('1BO8-BXaev2fKQ6M4OHfZpOE5lo-4s7el'),
+    standards: driveImage('1KggyALGnwz_yyI1at8LB61ymMUhv0hGQ'),
+    lifestyleFamily: driveImage('1olEJdbFM2NPyFkmXP98ELvLENJVUHrT9'),
+    lifestyleActive: driveImage('1TCcrCJe7eD6xB0dEkuK0FbmD0KfqHRD3'),
+    distribution: driveImage('1d90nFrBPJttVHQZk23qDkbeN0UPmU-aM'),
+    iraq: driveImage('1BNeXshDqQhU1tMVU7sHm2ISghSiDLZeh'),
+    cta: driveImage('1FDKQ7u-xNaRsnuHFX67Pf9k0Fj9n6rL5'),
+  },
+  products: {
+    group: driveImage('1XpRn1i-qeKpobA8ivlj38IA_d7TKElc7'),
+    front1000: driveImage('1Q0dryDXEK10QCE9nxlEj5wlgnnVKkT9_', 1800),
+    front500: driveImage('1bCX49Fz8pW0jCE2l5Dr1YiyXprMzhZI8', 1800),
+    front330: driveImage('1HTPbe3e55cVuamJOqxy8IGTWjdd9Hdab', 1800),
+    carton: driveImage('1iXV4EuCQdja7XgmJkUfucZXh_ZSRfwIQ'),
+    shrink: driveImage('1zGrsfbVexjb4Gm9sdCixVPoMgypPtbvi'),
+    detail: driveImage('1p8LgGWyriWiAcojzG3lB60FI2sx-fKTs'),
+    use: driveImage('15xvt6lZ5p875LCS9rCVUA2Pak39Zle_A'),
+  },
+  quality: {
+    hero: driveImage('1Qnp-mNO3-appynBH_ZGEfQI0lan9A-OU'),
+    lab: driveImage('1wHH06JQDrLEL2cOwkidvJviq_l6eSwvH'),
+    sample: driveImage('1yWtoM6R8Hfkt0zjMBMoX5g6020N5RtHM'),
+    hygiene: driveImage('1u5wc-3Y8nHMrXnFlo1ml3tiXfUeZYvRC'),
+    water: driveImage('1qMNLenollQMpfnHhHtPZJeUh_jBvNNfs'),
+    certificateVisual: driveImage('1dEgiVx0fY1HKpK31MUqYL1MISZIImxCo'),
+    factory: driveImage('14HozFX5fnZFNd_8KlShzsjx-z_JSh-Er'),
+  },
+  about: {
+    brand: driveImage('111Eq-wyd20KYn3ihxTh1-YG8KPtLJD_W'),
+    factory: driveImage('1XLDfxoU6yYBoAV1NbI5XeIw3suNgV_fl'),
+    team: driveImage('1YnxUPUzqMi9BP7lephODon_Oj71Ub5Ak'),
+    iraq: driveImage('1SdwdeWPxZvCU3EUo_NzjWjha_zbigx8O'),
+    operations: driveImage('15lMES6sub4HxoCSO50tmHMEhdXAOjdPQ'),
+  },
+  distribution: {
+    hero: driveImage('1ytQGxnymkw7Y1pBJObRcK3qGcB1Qni5R'),
+    warehouse: driveImage('1uAeQvN1UbkbBTZ5Jnii3OFhNO3UXRHIB'),
+    retail: driveImage('1CNa_rXk6iQOR0qo8jxbA9o_npxyXyPDj'),
+    loading: driveImage('1vIULUp-7R0YdpNZuxCONkiBkyw1du4jl'),
+    b2b: driveImage('1LhXq-1jxQiJWYgQg9-xq2JkCsMvj5IDp'),
+  },
+  contact: {
+    hero: driveImage('1CRyXYoRW6pYIlcbKKkqYiPCYiylJ2Fj5'),
+    b2b: driveImage('1cq_U8YuZjA57UJGROO_jst1fssveHQhD'),
+    location: driveImage('1KGiAxzIMYGF-d4PuLDJRl1KiGH00sFyx'),
+  },
+} as const;
+
+export { driveImage };
